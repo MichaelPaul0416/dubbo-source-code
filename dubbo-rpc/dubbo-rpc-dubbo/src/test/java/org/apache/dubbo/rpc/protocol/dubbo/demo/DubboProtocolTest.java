87c4a63049ef9853591dd90f19eb79216b1fe454
MichaelPaul0416 <18357903752m0@sina.cn>
2020-04-15 11:03:11 +0800

    dubbo协议基本调用

--- dubbo-rpc/dubbo-rpc-dubbo/src/test/java/org/apache/dubbo/rpc/protocol/dubbo/demo/DubboProtocolTest.java
+++ dubbo-rpc/dubbo-rpc-dubbo/src/test/java/org/apache/dubbo/rpc/protocol/dubbo/demo/DubboProtocolTest.java
@@ -1,8 +1,8 @@
 package org.apache.dubbo.rpc.protocol.dubbo.demo;
 
+import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.extension.ExtensionLoader;
-import org.apache.dubbo.rpc.Protocol;
-import org.apache.dubbo.rpc.ProxyFactory;
+import org.apache.dubbo.rpc.*;
 import org.junit.Test;
 
 /**
@@ -13,7 +13,7 @@ import org.junit.Test;
 public class DubboProtocolTest {
 
     @Test
-    public void dubboProtocol(){
+    public void dubboProtocol() {
         // 指定协议和代理对象
         Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
         ProxyFactory proxyFactory = ExtensionLoader.getExtensionLoader(ProxyFactory.class).getAdaptiveExtension();
@@ -21,11 +21,33 @@ public class DubboProtocolTest {
         // 指定接口
         RpcService rpcService = new RpcServiceImpl();
 
+        URL url = new URL("dubbo", "192.168.153.103", 9999);
         // 获取Invoker
-//        proxyFactory.getInvoker()
-        // 协议暴露
-//        protocol.export()
+        Invoker<RpcService> invoker = proxyFactory.getInvoker(rpcService, RpcService.class, url);
 
+        // 协议暴露
+        protocol.export(invoker);
+
+        // client操作
+        Invoker<RpcService> remoteInvoker = protocol.refer(RpcService.class, url);
+
+        // 代理获取接口代理对象，进行远程调用
+        RpcService proxyService = proxyFactory.getProxy(remoteInvoker);
+        String str = proxyService.doRpc("hello");
+        System.out.println("rpc call result:" + str);
+
+        // 也可以直接使用Invoker进行调用
+        RpcInvocation invocation = new RpcInvocation();
+        invocation.setInvoker(remoteInvoker);
+        invocation.setArguments(new String[]{"hello"});
+        invocation.setParameterTypes(new Class[]{String.class});
+        invocation.setMethodName("doRpc");
+        Result result = remoteInvoker.invoke(invocation);
+        System.out.println("invoker result -> " + result.getValue());
+
+        while (true){
+
+        }
     }
 
 }
